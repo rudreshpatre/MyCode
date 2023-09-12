d87e087348294205cb3bd2c74aa0b4cc6162fe26
Rudresh Patre <rudreshpatre@gmail.com>
2023-09-12 22:48:02 +0530

    Create CodeNotProudOf.cs
    In this code, I demonstrate past bad practices such as poor variable naming, excessive if-else nesting, lack of try-catch blocks, absence of dependency injection, and code repetition across methods, code which I am not proud of.

---
+++ CodeNotProudOf.cs
@@ -0,0 +1,48 @@
+using System;
+
+public class OrderProcessor
+{
+    public void ProcessOrder(Order order)
+    {
+        // Poor variable naming and excessive if-else statements
+        decimal cost = 0;
+        // Null checks missing
+        if (order.Quantity != null)
+        {
+            if (decimal.TryParse(order.Quantity.ToString(), out decimal quantity))
+            {
+                if (order.UnitPrice != null)
+                {
+                    if (decimal.TryParse(order.UnitPrice.ToString(), out decimal unitPrice))
+                    {
+                        // Repeating logic from other methods
+                        cost = quantity * unitPrice + (quantity * 2.0m);
+                        Console.WriteLine("Total cost: $" + cost);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid unit price.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unit price is missing.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid quantity.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Quantity is missing.");
+        }
+    }
+}
+
+public class Order
+{
+    public object Quantity { get; set; }
+    public object UnitPrice { get; set; }
+}
